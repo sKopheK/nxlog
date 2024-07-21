9b52426c53885e99e67976433f0addf091e5ab2b
skophek <libor@lipe.cz>
2024-07-21 18:39:07 +0200

    feat(Input): add copy password functionality

--- src/components/PasswordGenerator/Input.test.tsx
+++ src/components/PasswordGenerator/Input.test.tsx
@@ -1,11 +1,24 @@
-import { screen } from "@testing-library/dom";
-import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom"
+import { render } from "@testing-library/react"
 
-import Input from './Input';
+import Input from './Input'
+import userEvent from "@testing-library/user-event"
 
 it('should display input and copy button', () => {
     render(<Input value="123" />)
 
     screen.getByLabelText<HTMLInputElement>('Generated password')
     screen.getByTitle<HTMLButtonElement>('Copy password')
+})
+
+it('should copy input value to clipboard', async () => {
+    const value = 'mYS3cr3t'
+    const user = userEvent.setup()
+    render(<Input value={value} />)
+
+    const copyButton = screen.getByTitle<HTMLButtonElement>('Copy password')
+    await user.click(copyButton)
+
+    const clipboardText = await navigator.clipboard.readText()
+    expect(clipboardText).toBe(value)
 })
