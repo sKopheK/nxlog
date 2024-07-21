8f7ae7a1e71969a72d20615bc3dd903caa5fb542
skophek <libor@lipe.cz>
2024-07-21 18:15:59 +0200

    refactor: move generated password input to separate file

--- src/components/PasswordGenerator/index.tsx
+++ src/components/PasswordGenerator/index.tsx
@@ -8,6 +8,7 @@ import { generatePassword } from './utils';
 import LengthSlider from './LengthSlider';
 import Options from './Options';
 
+import Input from './Input';
 import styles from './styles.module.scss';
 
 const initialState: PasswordGeneratorState = {
@@ -38,15 +39,7 @@ const PasswordGenerator: FC = () => {
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.inputWrapper}>
-                <input type="text" aria-label="Generated password" value={password} readOnly className={styles.password} />
-                <button type="button" title="Copy password" className={styles.copyBtn}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-                        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-                    </svg>
-                </button>
-            </div>
+            <Input value={password} />
             <LengthSlider value={passwordLength} onChange={handleLengthChange} />
             <Options selected={options} setSelected={handleOnOptionChange} />
             <button type="button" onClick={handleGenerate}>Generate</button>
